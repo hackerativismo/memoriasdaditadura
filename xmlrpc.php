0638e9a1eaf1b32aa7caaeeb4472f8d84a854411
Valessio Brito <contato@valessiobrito.com.br>
2018-11-06 18:19:43 -0800

    wget -r ... 7/11/2018

---
+++ xmlrpc.php
@@ -0,0 +1,20 @@
+<br />
+<b>Warning</b>:  include(./wp-load.php): failed to open stream: No such file or directory in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>29</b><br />
+<br />
+<b>Warning</b>:  include(./wp-load.php): failed to open stream: No such file or directory in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>29</b><br />
+<br />
+<b>Warning</b>:  include(): Failed opening './wp-load.php' for inclusion (include_path='.:/usr/lib/php:/usr/local/lib/php') in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>29</b><br />
+<br />
+<b>Warning</b>:  include_once(ABSPATHwp-admin/includes/admin.php): failed to open stream: No such file or directory in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>62</b><br />
+<br />
+<b>Warning</b>:  include_once(): Failed opening 'ABSPATHwp-admin/includes/admin.php' for inclusion (include_path='.:/usr/lib/php:/usr/local/lib/php') in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>62</b><br />
+<br />
+<b>Warning</b>:  include_once(ABSPATHWPINC/class-IXR.php): failed to open stream: No such file or directory in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>63</b><br />
+<br />
+<b>Warning</b>:  include_once(): Failed opening 'ABSPATHWPINC/class-IXR.php' for inclusion (include_path='.:/usr/lib/php:/usr/local/lib/php') in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>63</b><br />
+<br />
+<b>Warning</b>:  include_once(ABSPATHWPINC/class-wp-xmlrpc-server.php): failed to open stream: No such file or directory in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>64</b><br />
+<br />
+<b>Warning</b>:  include_once(): Failed opening 'ABSPATHWPINC/class-wp-xmlrpc-server.php' for inclusion (include_path='.:/usr/lib/php:/usr/local/lib/php') in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>64</b><br />
+<br />
+<b>Fatal error</b>:  Call to undefined function apply_filters() in <b>/home/memoriasdaditadu/public_html/xmlrpc.php</b> on line <b>80</b><br />
